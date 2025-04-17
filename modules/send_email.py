2b5468b7f6bb94e9727f2f9e9a5f78722e047311
kaushalgadhiya <kaushalgadhiya04@gmail.com>
2025-04-17 14:05:10 +0530

    Update send_email.py
    Removed extra comments

--- modules/send_email.py
+++ modules/send_email.py
@@ -24,26 +24,3 @@ def send_email(to: str, subject: str, body: str):
     user = "me"  # Gmail API uses 'me' to refer to authenticated user
     message = create_message(sender=user, to=to, subject=subject, message_text=body)
     return send_message(service, user, message)
-
-# from  utils import gmail_auth
-# from email.mime.text import MIMEText
-# import base64
-
-
-# def create_message(to, subject, body):
-#     message = MIMEText(body)
-#     message["to"] = to
-#     message["subject"] = subject
-#     raw = base64.urlsafe_b64encode(message.as_bytes()).decode()
-#     return {"raw": raw}
-
-
-# def send_email(recipient_name, recipient_email, subject, body):
-#     try:
-#         service = gmail_auth.gmail_login()
-#         message = create_message(recipient_email, subject, body)
-#         send = service.users().messages().send(userId="me", body=message).execute()
-#         return True
-#     except Exception as e:
-#         print("Error:", e)
-#         return False
